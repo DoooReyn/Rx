c73a18b18b16c8581383b920758fc0ed2fbe4e40
Reyn <jl88744653@gmail.com>
2024-10-04 23:06:57 +0800

    RxGRUB

--- preview-template/RxGRUB.js
+++ preview-template/RxGRUB.js
@@ -1,4 +1,4 @@
-(globalThis || window || self || frames || global || this).RxSettings = {
+(globalThis || window || self || frames || global || this)._RxSettings = {
     mode: "dev", // "dev" or "dist"
     res_server: "http://127.0.0.1:7777/resources/",
 };

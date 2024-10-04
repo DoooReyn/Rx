82bbb516bf34fedefc3c81ec731ed77a2c725338
Reyn <jl88744653@gmail.com>
2024-10-04 22:52:02 +0800

    Revert "ddd"
    This reverts commit 79371b99a7a0be7982bfb65f7162915ecc0a16e4.

--- preview-template/settings.js
+++ preview-template/settings.js
@@ -1,4 +1,4 @@
-(globalThis || window || self || frames || global || this)._RxSettings = {
-    env: "dev", // "dev" or "dist"
+(globalThis || window || self || frames || global || this).RxSettings = {
+    mode: "dev", // "dev" or "dist"
     res_server: "http://127.0.0.1:7777/resources/",
 };

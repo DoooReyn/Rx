e2fae34c9410b01f8663bd6144f30cb7bf8d27a8
Reyn <jl88744653@gmail.com>
2024-10-05 10:19:39 +0800

    设备屏幕

--- assets/package/boot/source/Entry.ts
+++ assets/package/boot/source/Entry.ts
@@ -1,27 +1,35 @@
-import { Game, assetManager, director, game, log } from "cc";
+import { Game, assetManager, director, game } from "cc";
 import { EDITOR, PREVIEW } from "cc/env";
+
+
 
 if (!EDITOR) {
     const scene = "boot";
+    const next_scene = "game";
 
     game.once(Game.EVENT_ENGINE_INITED, function () {
-        log("引擎初始化完成");
+        console.debug = console.debug.bind(console, "[Rx.D]");
+        console.log = console.log.bind(console, "[Rx.L]");
+        console.info = console.info.bind(console, "[Rx.I]");
+        console.warn = console.warn.bind(console, "[Rx.W]");
+        console.error = console.error.bind(console, "[Rx.E]");
+        console.log("引擎初始化完成");
     })
 
     game.once(Game.EVENT_GAME_INITED, function () {
-        log("系统初始化完成");
+        console.log("系统初始化完成");
 
         director.loadScene(scene, function () {
-            log(scene, "场景加载完成");
+            console.log(scene, "场景加载完成");
             let version = "";
             if (!PREVIEW) {
                 const url = [_RxSystemSettings.res_server, "versions.json"].join("/");
                 assetManager.downloader.appendTimeStamp = true;
                 assetManager.loadRemote(url,)
             }
-            assetManager.loadBundle("game", { version }, function (err, bun) {
-                director.loadScene("game", function (err, scene) {
-                    console.log('--->', err, scene);
+            assetManager.loadBundle(next_scene, { version }, function (err, bun) {
+                director.loadScene(next_scene, function (err, scene) {
+                    console.log('', err, scene);
                 })
             })
         });

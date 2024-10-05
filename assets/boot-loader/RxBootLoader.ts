9cc774358cfcac0d81bcc8fb3de3a2bb40e5a6b2
Reyn <jl88744653@gmail.com>
2024-10-05 10:36:33 +0800

    BootLoader

--- assets/boot-loader/RxBootLoader.ts
+++ assets/boot-loader/RxBootLoader.ts
@@ -1,18 +1,16 @@
 import { Game, assetManager, director, game } from "cc";
 import { EDITOR, PREVIEW } from "cc/env";
 
-
-
 if (!EDITOR) {
     const scene = "boot";
     const next_scene = "game";
 
     game.once(Game.EVENT_ENGINE_INITED, function () {
-        console.debug = console.debug.bind(console, "[Rx.D]");
-        console.log = console.log.bind(console, "[Rx.L]");
-        console.info = console.info.bind(console, "[Rx.I]");
-        console.warn = console.warn.bind(console, "[Rx.W]");
-        console.error = console.error.bind(console, "[Rx.E]");
+        console.debug = console.debug.bind(console, "%c [Rx.D] ", "background-color:rgb(121,121,121);color:white;");
+        console.log = console.log.bind(console, "%c [Rx.L] ", "background-color:rgb(52,52,52);color:white;");
+        console.info = console.info.bind(console, "%c [Rx.I] ", "background-color:rgb(61,132,247);color:white;");
+        console.warn = console.warn.bind(console, "%c [Rx.W] ", "background-color:rgb(234,166,68);color:white;");
+        console.error = console.error.bind(console, "%c [Rx.E] ", "background-color:rgb(231,74,97);color:white;");
         console.log("引擎初始化完成");
     })
 
